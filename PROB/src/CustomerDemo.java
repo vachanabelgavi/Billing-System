cac9ba7a4138ee2e8de484bf629a83f12f90b764
vachanabelagavi <vachanabelagavi777@gmail.com>
2016-09-28 14:50:25 +0530

    PROBLEM1

---
+++ PROB/src/CustomerDemo.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class CustomerDemo {
+	public static void main(String[] args) {
+		
+		Scanner sc =new Scanner(System.in);
+		Scanner sc1 =new Scanner(System.in);
+	   System.out.println("ENTER THE CUSTOMER NAME");
+	   String sCustName=sc.nextLine();
+	   System.out.println("ENTER THE CUSTOMER ID");
+	   int iCustID=sc.nextInt();
+	   System.out.println("ENTER THE CUSTOMER PHONE NUMBER");
+	   long lTelephone=sc.nextLong();
+	   System.out.println("ENTER ADDRESS");
+	   String sAddress=sc1.nextLine();
+	   
+	   Customer vss=new Customer(iCustID, sCustName, sAddress, lTelephone);
+	   
+	   System.out.println("ENTER THE ITEM ID");
+	   int iItemID=sc.nextInt();
+	   ItemDetails item=new ItemDetails(iItemID);
+	   item.validateItemID();
+	   
+	   System.out.println("ENTER THE QUANTITY");
+	   int iQty=sc.nextInt();
+	   PurchaseBill p1=new PurchaseBill(iQty);
+	   p1.calculateBill(item);
+	   
+	   
+	   vss.displayDetails();
+	   item.displayDetails();
+	   p1.Display();
+	   if(vss.validateCustomerName()==false)
+		   System.out.println("INVALID NAME");
+	   if(vss.validateTelephoneNo()==false)
+		   System.out.println(" INVALID PHONE NUMBER");
+	}
+
+}
